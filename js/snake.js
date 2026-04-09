dd8e88dfe8e314ebeea4726e6a82b2b797f11f66
Mall3000 <joeylee6873@gmail.com>
2026-04-09 23:04:42 +0800

    NEw snake

--- js/snake.js
+++ js/snake.js
@@ -1,29 +1,105 @@
 const canvas = document.getElementById("snake_game")
 const ctx = canvas.getContext("2d")
-const box = 20 // how big the gamme is
+const box = 20 // how big the gamme is (in movable squares)
+// DO NOTE THAT THE GAME IS 100PX
 let apple = {x:10,y:10}
 let skp = [
-    {x:10,y:10}
+    {x:2,y:10},
+    {x:3,y:10},
+    {x:4,y:10}
 ]
 function applefunc() {
-    apple.x = (Math.random()*20).floor()
-    apple.y = (Math.random()*20).floor()
+    apple.x=Math.floor(Math.random()*20)
+    apple.y=Math.floor(Math.random()*20)
 }
-let dir;
+let dir = "right";
 document.addEventListener('keydown',direction)
+function dircalc(key) {
+    if (key == 37 && dir!="right") {return "left"}
+    if (key == 40 && dir!="down") {return "up"}
+    if (key == 39 && dir!="left") {return "right"}
+    if (key == 38 && dir!="up") {return "down"}
+    return dir
+}
 function direction(press) {
     let key = press.keyCode
-    if (key == 37 && dir!="right") dir="left"
-    if (key == 38 && dir!="down") dir="up"
-    if (key == 39 && dir!="left") dir="right"
-    if (key == 40 && dir!="up") dir="down"
+    if (buffer=1) {
+        
+        dir=dircalc(key)
+        
+        buffer=0
+    }
+}
+function restart() {
+    apple={x:10,y:10}
+    buffer = 0
+    skp = [
+    {x:2,y:10},
+    {x:3,y:10},
+    {x:4,y:10}
+    ],
+    dir = "right";
 }
 // draws game
 function draw() {
-    ctx.fillStyle = "#000000"
+    ctx.fillStyle = "#111133"
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-    for (let i = 0; i < skp.length(); i++) {
-    ctx.fillStyle = (i == 0) ? "green" : "white";
-    ctx.fillRect(snake[i].x * box, snake[i].y * box, box, box);
-  }
+    // snake
+    ctx.fillStyle = "#337733"
+    for (let i = 0; i < skp.length; i++) {
+        
+        ctx.fillRect(skp[i].x * 5, skp[i].y * 5, 5,5);
+    }
+    // food
+    ctx.fillStyle = "red";
+    ctx.fillRect(apple.x*5,apple.y*5,5,5);
+
+    
+    
+
 }
+function calcnewloc() {
+
+    let newp=Object.assign({},skp[skp.length-1]);
+        switch (dir) {
+            
+            case "right":{
+                
+                newp.x=newp.x+1
+                return newp
+            }
+            case "left":{
+                
+                newp.x=newp.x-1
+                return newp
+            }
+
+            case "up":{
+                
+                newp.y=newp.y+1
+                return newp
+            }
+            case "down":{
+                
+                newp.y=newp.y-1
+                return newp
+            }
+        }
+}
+function game() {
+    
+    skp.shift()
+    // movemenet of snake
+    skp.push(calcnewloc())
+
+    // snake eats apple
+    if (skp.some(obj =>
+        obj.x === apple.x && obj.y === apple.y
+    )) {applefunc();skp.push(calcnewloc())}
+}
+
+const snaiklop = setInterval(() => {
+    game()
+    draw()
+    buffer=1
+}, 170);
